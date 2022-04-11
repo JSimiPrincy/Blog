ea50977bc25d6511e6bd083c212b7c02f291cf05
simiprincy <83680562+JSimiPrincy@users.noreply.github.com>
2022-04-11 07:07:13 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 import axios from "axios"
  export const axiosInstance = axios.create({
-     baseURL: "https://blog-roan-six-28.vercel.app/"
+     baseURL: "https://625385e5b201ff6ac3437596--silver-halva-75470b.netlify.app/api"
  })
